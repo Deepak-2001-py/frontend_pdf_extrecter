6fbfc3e45916c5c9c08631c3bccf412ae6d92717
deepak Rajbhar <2001deepakrajbhar@gmail.com>
2025-02-06 17:37:50 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000, // or another port of your choice
+      host: '0.0.0.0', // Listen on all network interfaces
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000', // Your backend URL
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
 });
